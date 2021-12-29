b5d3dab59cf9c6664f7b6a1c5202ee2959f4412a
hypensanez12 <46888854+hypensanez12@users.noreply.github.com>
2021-12-29 20:54:52 +0000

    Started

---
+++ Banking-Application-master/Banking-Application-master/Banking Application/Account_Type.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking_Application
+{
+    class Account_Type
+    {
+        public static int Current_Account = 1;
+        public static int Savings_Account = 2;
+    }
+}
